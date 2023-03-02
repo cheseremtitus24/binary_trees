043c0c3fc67cb36599b636463c02dc1b911ca815
Cheserem Titus <cheseremtitus24@gmail.com>
2023-03-02 13:30:26 +0300

    new file:   17-binary_tree_sibling.c 	new file:   17-main.c 	new file:   17.sh

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling- Finds Other Pair @ same depth
+ * @node: node to find level Pair 
+ *
+ * Return: node_pair pointer if exists else NULL 
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+	/* If current node is the root node then it has no siblings*/
+
+	if (!node->parent)
+		return (NULL);
+
+	/* if current node is left return the right sibling*/
+	if (node == node->parent->left)
+	{
+			return (node->parent->right);
+	}
+
+	return (node->parent->left);
+		
+}
+
+
---
+++ 17.sh
@@ -0,0 +1,1 @@
+gcc -g -Wall -Wextra -Werror -pedantic binary_tree_print.c 17-main.c 17-binary_tree_sibling.c 0-binary_tree_node.c -o 17-sibling && ./17-sibling
