d4c589cc0317740a930849f4da4ffca50fb58c5d
Cheserem Titus <cheseremtitus24@gmail.com>
2023-03-02 07:01:24 +0300

    modified:   8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -13,9 +13,9 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 	}
 	/* traverse left*/
-	binary_tree_postorder(tree->left,func);
+	binary_tree_postorder(tree->left, func);
 	/* traverse right*/
-	binary_tree_postorder(tree->right,func);
+	binary_tree_postorder(tree->right, func);
 	/* traverse root*/
 	func(tree->n);
 }
