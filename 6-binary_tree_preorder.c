ccd468a08ed05455ffdae495d6559fce6e4c3b4b
Cheserem Titus <cheseremtitus24@gmail.com>
2023-03-02 13:58:56 +0300

    modified:   6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -10,6 +10,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	/* node should not be NULL*/
 	if (!tree || !func)
 	{
+		return;
 	}
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
