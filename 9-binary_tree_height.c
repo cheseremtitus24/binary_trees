a262a55e8dc97ef0b7dd3666c0c1b0804a8e63e1
Cheserem Titus <cheseremtitus24@gmail.com>
2023-03-02 07:02:42 +0300

    modified:   9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,6 +7,7 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
+
 	left_height = 0;
 	right_height = 0;
 
@@ -19,9 +20,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	else
 	{
 		/* traverse left*/
-		left_height = tree->left ? 1 + binary_tree_height(tree->left):0;
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		/* traverse right*/
-		right_height = tree->right ? 1 + binary_tree_height(tree->right):0;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 		if (left_height >= right_height)
 			return (left_height);
